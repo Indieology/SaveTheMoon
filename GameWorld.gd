6d5ac78fde93a0a12aee0555deb6ac50430f9d11
Indieology <contactindieology@gmail.com>
2022-08-15 16:09:45 -0500

    move player logic to player script

--- GameWorld.gd
+++ GameWorld.gd
@@ -1,44 +1,6 @@
 extends Node2D
 
-export var player_speed : int = 60
-
-onready var pivot = $Pivot
-onready var player = $Pivot/Player
-onready var player_gun = $Pivot/Player/Gun
-
-onready var bullet = preload("res://Player/Bullet.tscn")
-
 func _ready():
-	player.play("Ready")
-
-func _physics_process(delta):
-	if Input.is_action_pressed("move_left"):
-		move_left(delta)
-	elif Input.is_action_pressed("move_right"):
-		move_right(delta)
-	else:
-		if $Pivot/Player/ReadyTimer.is_stopped():
-			player.play("Idle")
-	
-	if Input.is_action_just_pressed("shoot"):
-		var this_bullet = bullet.instance()
-		get_node("Bullets").add_child(this_bullet)
-		this_bullet.global_position = player_gun.global_position
-		
-		if player.flip_h == true:
-			this_bullet.bullet_speed *= -1
-		
-
-
-func move_left(delta):
-	pivot.rotation_degrees -= player_speed * delta
-	player.flip_h = true
-	player.play("Walk")
-
-func move_right(delta):
-	pivot.rotation_degrees += player_speed * delta
-	player.flip_h = false
-	player.play("Walk")
-
+	pass
 
 #-270 -160
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,46 @@
 extends AnimatedSprite
 
+export var player_speed : int = 60
+
+onready var pivot = get_parent()
+onready var player_start_timer = $ReadyTimer
+onready var player_gun = $Gun
+
+onready var bullet = preload("res://Player/Bullet.tscn")
+
+func _ready():
+	play("Ready")
 
 func _process(delta):
 	if flip_h:
-		$Gun.position.x = -7.75
+		player_gun.position.x = -7.75
 	else:
-		$Gun.position.x = 7.75
+		player_gun.position.x = 7.75
+
+func _physics_process(delta):
+	if Input.is_action_pressed("move_left"):
+		move_left(delta)
+	elif Input.is_action_pressed("move_right"):
+		move_right(delta)
+	else:
+		if player_start_timer.is_stopped():
+			play("Idle")
+	
+	if Input.is_action_just_pressed("shoot"):
+		var this_bullet = bullet.instance()
+		get_parent().get_parent().get_node("Bullets").add_child(this_bullet)
+		this_bullet.global_position = player_gun.global_position
+		
+		if flip_h == true:
+			this_bullet.bullet_speed *= -1
+		
+
+func move_left(delta):
+	pivot.rotation_degrees -= player_speed * delta
+	flip_h = true
+	play("Walk")
+
+func move_right(delta):
+	pivot.rotation_degrees += player_speed * delta
+	flip_h = false
+	play("Walk")
